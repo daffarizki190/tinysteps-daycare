ef6009749be06e8feebdb562f51ebf16f6f1ce91
daffarizki190 <dafarizki209@gmail.com>
2026-07-23 23:30:49 +0700

    fix: restore standard android build.gradle layout.buildDirectory

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,10 +5,12 @@ allprojects {
     }
 }
 
-rootProject.buildDir = java.io.File("C:/Users/Public/Day_Care_Build")
+val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
+rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
-    project.buildDir = java.io.File(rootProject.buildDir, project.name)
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
 subprojects {
     project.evaluationDependsOn(":app")
